04a2c87f4d40bf9e4ef1b4ad14d868d9f69aa929
Jan-Kiezun <j4n3k777@gmail.com>
2023-01-20 16:52:20 +0100

    up to exercise 8

--- app.py
+++ app.py
@@ -40,7 +40,6 @@ async def get_all_employees():
         whereClause = f'WHERE n.{filter_key}="{filter_value}"' if filter_option else ""
         orderClause = "ORDER BY n." + sort_key if sort_key else ""
         query = f"MATCH (n:Employee) {whereClause} RETURN n {orderClause}"
-        print(query)
 
         coroutine = await session.run(query)
         data = await coroutine.data()
@@ -58,7 +57,7 @@ async def create_employee():
     department = data["department"]
     position = data["position"]
     async with driver.session() as session:
-        query = "MATCH (n:Employee {firstname:$firstname,lastname:$lastname,age:$age,salary:$salary,department:$department,position:$position}) MERGE (d:Department {name:$department}) MERGE (n)-[:WORKS_IN]->(d)"
+        query = "MERGE (n:Employee {firstname:$firstname,lastname:$lastname,age:$age,salary:$salary,department:$department,position:$position}) MERGE (d:Department {name:$department}) MERGE (n)-[:WORKS_IN]->(d)"
         await session.run(
             query,
             firstname=firstname,
@@ -69,4 +68,63 @@ async def create_employee():
             position=position,
         )
         await session.close()
-        return "Employee created successfully"
+        return "Employee creation passed"
+
+
+@app.route("/employees/<id>", methods=["PUT"])
+async def update_employee(id):
+    data = request.get_json()
+    firstname = data.get("firstname", None)
+    lastname = data.get("lastname", None)
+    age = data.get("age", None)
+    salary = data.get("salary", None)
+    department = data.get("department", None)
+    position = data.get("position", None)
+    print(data, id)
+    async with driver.session() as session:
+        query = f"MATCH (n:Employee) WHERE ID(n) = {id} SET n.firstname = '{firstname}', n.lastname = '{lastname}', n.age = {age}, n.salary = {salary}, n.department = '{department}', n.position = '{position}'"
+        await session.run(query)
+        await session.close()
+        return "Update went through"
+
+
+@app.route("/employees/<id>", methods=["DELETE"])
+async def delete_employee(id):
+    async with driver.session() as session:
+        query = f"MATCH (n:Employee) WHERE ID(n) = {id} DETACH DELETE n"
+        await session.run(query)
+        await session.close()
+        return "Delete went through"
+
+
+@app.route("/employees/<id>/subordinates", methods=["GET"])
+async def get_subordinates(id):
+    async with driver.session() as session:
+        query = f"MATCH (manager:Employee)-[:MANAGES]->(:Department)<-[:WORKS_IN]-(subordinate:Employee) WHERE ID(manager) = {id} AND ID(subordinate) <> {id} RETURN subordinate"
+        coroutine = await session.run(query)
+        data = await coroutine.data()
+        await session.close()
+        return str(data)
+
+
+@app.route("/employees/<id>/departmentInfo", methods=["GET"])
+async def get_department_info(id):
+    department_info = []
+    async with driver.session() as session:
+        query = f"MATCH (e:Employee)-[:WORKS_IN]->(department:Department) WHERE ID(e) = {id} RETURN department"
+        coroutine = await session.run(query)
+        data = await coroutine.data()
+        department_info.append(data[0])
+
+        query = f"MATCH (e:Employee)-[:WORKS_IN]->(department:Department)<-[:MANAGES]-(manager:Employee) WHERE ID(e) = {id} RETURN manager"
+        coroutine = await session.run(query)
+        data = await coroutine.data()
+        department_info.append(data[0])
+
+        query = f"MATCH (e:Employee)-[:WORKS_IN]->(department:Department)<-[:WORKS_IN]-(subordinate:Employee) WHERE ID(e) = {id} RETURN count(subordinate)+1 as count"
+        coroutine = await session.run(query)
+        data = await coroutine.data()
+        department_info.append(data)
+
+        await session.close()
+    return str(department_info)
